11fdd7c80201c657e575f1eb1d8e81d4c2b070b2
Lucas do Nascimento Tavares <Ltv2234@gmail.com>
2022-02-28 11:07:11 -0300

    feat: implementação de loops e condicionais

--- Aulas/modulo 1/parte 1/server.js
+++ Aulas/modulo 1/parte 1/server.js
@@ -1,1 +1,37 @@
-console.log("update")
+const numero = 100;
+const mutiplos = [];
+const par = [];
+const impar = [];
+
+//loop para numeros multiplos de 3 e 5
+for (let i = 0; i < numero; i++) {
+    if ((i % 3 === 0) || (i % 5 === 0)) {
+        mutiplos.push(i);
+    }
+}
+
+//loop para numeros pares
+for (let i = 0; i <= numero; i++) {
+    if (i % 2 === 0) {
+        par.push(i);
+    }
+}
+
+//loop para numeros impares
+for (let i = 0; i <= numero; i++) {
+    if (i % 2 === 1) {
+        impar.push(i);
+    }
+}
+
+//retorno de numeros mutiplos de 3 e 5
+console.log("mutiplos de 3 e 5")
+console.log(mutiplos);
+
+//retorno de numeros pares
+console.log("numéros pares")
+console.log(par);
+
+//retorno de numeros impares
+console.log("numéros impares")
+console.log(impar);
