fee96435d4820d7a7c8e1134f862fe167645db0b
lucas2799 <Ltv2234@gmail.com>
2022-03-06 16:37:56 -0300

    importações de exportações de modulos criados

---
+++ Aulas/modulo 1/parte 2/modules.js
@@ -0,0 +1,20 @@
+function soma(a, b) {
+    return a + b;
+}
+
+function subtracao(a, b) {
+   
+        return a - b;
+    
+    }
+
+function mutiplicacao(a, b) {
+    return a * b;
+}
+
+function divisao(a, b) {
+    return a / b;
+}
+
+//exportando modulo e seus objetos 
+module.exports = { soma, subtracao, mutiplicacao, divisao };
