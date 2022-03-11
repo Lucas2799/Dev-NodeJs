a0f668776caf162d1fbb4bc28eccee17e61749e9
Lucas do Nascimento Tavares <Ltv2234@gmail.com>
2022-03-11 09:07:56 -0300

    ajsuste model imports getall

--- Aulas/modulo 1/Desafio/projectdesafio/controllers/Pedidos.controller.js
+++ Aulas/modulo 1/Desafio/projectdesafio/controllers/Pedidos.controller.js
@@ -1,8 +1,8 @@
-import PedidosService from "../services/pedidos.service.js"
+import pedidosServices from "../services/pedidos.services.js";
 
 async function AllPedidos(req, res, next) {
     try {
-        res.send({ "mostModels": await PedidosService.AllPedidos() });
+        res.send({ "mostModels": await pedidosServices.AllPedidos()});
     } catch (err) {
         next(err);
     }
