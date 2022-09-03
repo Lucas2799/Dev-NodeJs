316932a7a15dea458a1e15fd5d766f291b29b3b2
lucas2799 <Ltv2234@gmail.com>
2022-09-03 11:13:03 -0300

    concatenação de escrita

--- Aulas/modulo 1/parte 2/filesystem/server.js
+++ Aulas/modulo 1/parte 2/filesystem/server.js
@@ -1,15 +1,23 @@
-import fs from "fs";
+import fs from "fs";//import modulo
 
-fs.writeFile("teste.txt", "tete", function(err){
+fs.writeFile("teste.txt", "tete", function(err){//criação de arquivo com nome e conteudo
     if (err){
-        console.log(err);
+        console.log(err);//retorno caso erro
     }else{
-        fs.readFile("teste.txt", "utf-8", function(err, data){
-         if (err){
-            console.log(err);
-         }else{
-            console.log(data);
-         }
-        })
+        fs.appendFile("teste.txt", " concatenado 1", function (err){ //concatenação de escrita 
+            if(err){
+                console.log(err);
+            }else{
+                fs.readFile("teste.txt", "utf-8", function(err, data){//leitura e formatação de arquivo
+                    if (err){
+                       console.log(err);
+                    }else{
+                       console.log(data);//imprimindo conteudo do arquivo
+                    }
+                   });  
+            }
+        });
+
+       
     }
-})
+});
