62e5d76baf32017298236f55e2874c0bfe5f33b4
lucas2799 <Ltv2234@gmail.com>
2022-09-08 10:17:22 -0300

    Roteamento com interrogação

--- Aulas/modulo 1/parte 2/express/server.js
+++ Aulas/modulo 1/parte 2/express/server.js
@@ -6,5 +6,10 @@ app.get("/", (req, res)=>{
     res.send("Olá Mundo!"); 
 });
 
+//Ao usar a interrogação em rota a mesma responderá com ou sem a ultima letra da rota 
+app.get("/teste?",(req,res)=>{
+    res.send("Teste pontuação em rota ")
+})
+
 app.listen(3000,()=>
 console.log("API Started") );
