19a7c3c0b924edb65627690d4fd8ed2b0354c5bf
lucas2799 <Ltv2234@gmail.com>
2022-09-11 08:58:08 -0300

    parametro de requisição ID

--- Aulas/modulo 1/parte 2/express/server.js
+++ Aulas/modulo 1/parte 2/express/server.js
@@ -8,18 +8,26 @@ app.get("/", (req, res)=>{
 
 //Ao usar a interrogação em rota a mesma responderá com ou sem a ultima letra da rota 
 app.get("/teste?",(req,res)=>{
-    res.send("Teste pontuação interrogação em rota ")
-})
+    res.send("Teste pontuação interrogação em rota ");
+});
 
 //Ao usar o ponto de + na rota o servidor aceita a repetição de ifitos do mesmo para a resposta 
 app.get("/teste+",(req,res)=>{
-    res.send("Teste pontuação adição em rota ")
-})
+    res.send("Teste pontuação adição em rota ");
+});
 
 //Ao usar o pondo de * o mesmo aceitará qualquer string em seu lugar
 app.get("/meu*emprego",(req,res)=>{
-    res.send("Teste pontuação * em rota ")
-})
+    res.send("Teste pontuação * em rota ");
+    console.log(req.path);
+});
+
+//roteamento de parametros
+
+//requisita um id 
+app.get("/teste/:id",(req,res)=>{
+    res.send(req.params.id);
+});
 
 app.listen(3000,()=>
 console.log("API Started") );
