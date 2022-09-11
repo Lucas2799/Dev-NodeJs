6e101a63d75fc492c2603a178513829ee19b354a
lucas2799 <Ltv2234@gmail.com>
2022-09-11 08:38:24 -0300

    Roteamento com *

--- Aulas/modulo 1/parte 2/express/server.js
+++ Aulas/modulo 1/parte 2/express/server.js
@@ -8,12 +8,17 @@ app.get("/", (req, res)=>{
 
 //Ao usar a interrogação em rota a mesma responderá com ou sem a ultima letra da rota 
 app.get("/teste?",(req,res)=>{
-    res.send("Teste pontuação em rota ")
+    res.send("Teste pontuação interrogação em rota ")
 })
 
 //Ao usar o ponto de + na rota o servidor aceita a repetição de ifitos do mesmo para a resposta 
 app.get("/teste+",(req,res)=>{
-    res.send("Teste pontuação em rota ")
+    res.send("Teste pontuação adição em rota ")
+})
+
+//Ao usar o pondo de * o mesmo aceitará qualquer string em seu lugar
+app.get("/meu*emprego",(req,res)=>{
+    res.send("Teste pontuação * em rota ")
 })
 
 app.listen(3000,()=>
